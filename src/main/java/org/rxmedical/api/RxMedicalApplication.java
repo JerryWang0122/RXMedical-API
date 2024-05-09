9ebdce5d24daf84ca8b9fffa7ca707371bf78161
JerryWang0122 <school832@gmail.com>
2024-05-09 17:11:04 +0800

    Project init

---
+++ src/main/java/org/rxmedical/api/RxMedicalApplication.java
@@ -0,0 +1,13 @@
+package org.rxmedical.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxMedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxMedicalApplication.class, args);
+    }
+
+}
