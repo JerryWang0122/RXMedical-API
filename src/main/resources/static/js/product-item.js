04af35e022eca194badf410df49d64cb650e31c2
JerryWang0122 <school832@gmail.com>
2024-05-16 12:02:49 +0800

    Front feat: 個別商品申請頁 sweet alert2 互動

--- src/main/resources/static/js/product-item.js
+++ src/main/resources/static/js/product-item.js
@@ -1,26 +1,62 @@
 $(document).ready(function () {
 
-  var jAmountInput = $('#jAmount');
-  var increaseBtn = $('#jAmountIncreaseBtn');
-  var decreaseBtn = $('#jAmountDecreaseBtn');
+  // ---------------- 申請量增減按鈕互動 --------------------------
+  const jAmountInput = $('#jAmount');
+  const increaseBtn = $('#jAmountIncreaseBtn');
+  const decreaseBtn = $('#jAmountDecreaseBtn');
+
+  const addToCartBtn = $('#jAddToCartBtn');
 
   // jAmount 输入框的输入事件处理
   jAmountInput.on('input', function () {
-    var value = $(this).val().replace(/\D/g, '');
+    const value = $(this).val().replace(/\D/g, '');
     $(this).val(value);
   });
 
   // 增加按钮点击事件处理
   increaseBtn.on('click', function () {
-    var currentValue = parseInt(jAmountInput.val()) || 0;
+    const currentValue = parseInt(jAmountInput.val()) || 0;
     jAmountInput.val(currentValue + 1);
   });
 
   // 减少按钮点击事件处理
   decreaseBtn.on('click', function () {
-    var currentValue = parseInt(jAmountInput.val()) || 2;
+    const currentValue = parseInt(jAmountInput.val()) || 2;
     if (currentValue > 1) {
       jAmountInput.val(currentValue - 1);
     }
   });
+
+  // ---------------- 按下放入購物車時的動作 ------------------------
+  const handleAddToCartBtnClick = (event) => {
+    event.preventDefault();
+
+    // TODO: 申請單項目資料
+    const formData = {};
+
+    // TODO: 檢查商品數量是否大於庫存量
+    const hasProductInStock = true;
+
+    if (hasProductInStock) {
+      // TODO: 放到localStorage
+      Swal.fire({
+        position: "top",
+        icon: "success",
+        title: "已加入購物車",
+        showConfirmButton: false,
+        timer: 1500
+      })
+      
+    } else {
+      Swal.fire({
+        title: "加入失敗",
+        icon: "error"
+      })
+    }
+
+  };
+
+  // addToCartBtn.on('submit', handleAddToCartBtnClick);
+  addToCartBtn.on('click', handleAddToCartBtnClick);
+
 });
