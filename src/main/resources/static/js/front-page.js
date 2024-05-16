48846d05300d4caeea566866f99d4fe30f1564c5
JerryWang0122 <school832@gmail.com>
2024-05-16 16:28:12 +0800

    Front feat: 除了個別商品的部分，全部用js分流

--- src/main/resources/static/js/front-page.js
+++ src/main/resources/static/js/front-page.js
@@ -10,9 +10,36 @@ const loadHTML = async(url, target) => {
   } catch (err) {
     console.error(err);
   }
-  
+
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
+const handleChangeNavBar = async (event, idName, url) => {
+  const eventId = '#' + event.target.id;
+  
+  // if eventId not equals to idName => return
+  if (eventId !== idName) {
+    return;
+  }
+
+  $('#navbarMenu .navbar-nav .nav-link').removeClass('active');
+
+  // 将点击的按钮添加 'active' 类
+  $(event.target).addClass('active');
+  await loadHTML(url, '#contentArea');
+
+};
+
+// 待 DOM 加載完成之後再執行
+$(document).ready(async() => {
   await loadHTML('/html/f-products.html', '#contentArea');
+
+  // 按下navbar的標題時，回到首頁
+  $('#frontIndexBtn').on('click', () => location.reload());
+  
+  // -------------------- navbar 按鈕被按下時 -----------------
+  $('#navbarMenu').on('click', async(event) => {
+    await handleChangeNavBar(event, '#myAccountBtn', '/html/f-user_info.html');
+    await handleChangeNavBar(event, '#applyListBtn', '/html/f-user_info.html');
+    await handleChangeNavBar(event, '#backManageBtn', '/html/f-user_info.html');
+  })
 });
