4c4ce130874839d379b0daa1d6d21ee4b7d0faed
JerryWang0122 <school832@gmail.com>
2024-05-16 15:22:03 +0800

    Front feat: 產品頁用js讀取內容

--- src/main/resources/static/js/front-page.js
+++ src/main/resources/static/js/front-page.js
@@ -1,3 +1,18 @@
-$(document).ready(function () {
+const BASE_URL = '';
+
+const loadHTML = async(url, target) => {
+  const fullURL = BASE_URL + url;
+
+  try {
+    const response = await fetch(fullURL);
+    const data = await response.text();
+    $('#contentArea').html(data);
+  } catch (err) {
+    console.error(err);
+  }
   
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadHTML('/html/f-products.html', '#contentArea');
 });
