318e78db80e6f6699712033b4d8132cc6aeeaf06
JerryWang0122 <school832@gmail.com>
2024-05-17 09:38:18 +0800

    Front feat: 5/17更新當下最新版

--- src/main/resources/static/js/front-page.js
+++ src/main/resources/static/js/front-page.js
@@ -31,15 +31,15 @@ const handleChangeNavBar = async (event, idName, url) => {
 
 // 待 DOM 加載完成之後再執行
 $(document).ready(async() => {
-  await loadHTML('/html/f-products.html', '#contentArea');
+  await loadHTML('/f-products.html', '#contentArea');
 
   // 按下navbar的標題時，回到首頁
   $('#frontIndexBtn').on('click', () => location.reload());
   
   // -------------------- navbar 按鈕被按下時 -----------------
   $('#navbarMenu').on('click', async(event) => {
-    await handleChangeNavBar(event, '#myAccountBtn', '/html/f-user_info.html');
-    await handleChangeNavBar(event, '#applyListBtn', '/html/f-user_info.html');
-    await handleChangeNavBar(event, '#backManageBtn', '/html/f-user_info.html');
+    await handleChangeNavBar(event, '#myAccountBtn', '/f-user_info.html');
+    await handleChangeNavBar(event, '#applyListBtn', '/f-user_info.html');
+    await handleChangeNavBar(event, '#backManageBtn', '/f-user_info.html');
   })
 });
--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
   // 当用户点击除了“全部”之外的分类复选框时
   $('.categories input[type="checkbox"]').not('#cate-all').on('change', function () {
+    alert('aaaaa');
     // 如果点击的不是“全部”复选框
     if ($(this).attr('id') !== 'cate-all') {
       // 取消选中“全部”复选框
@@ -11,6 +12,7 @@ $(document).ready(function () {
   
   // 当用户点击“全部”复选框时
   $('#cate-all').on('change', function () {
+    alert('aaaaa');
     // 如果“全部”复选框被选中
     if ($(this).prop('checked')) {
       // 取消其他复选框的选中状态
@@ -23,6 +25,7 @@ $(document).ready(function () {
   
   // 当任何分类复选框被点击时
   $('.categories input[type="checkbox"]').not('#cate-all').on('change', function () {
+    alert('aaaaa');
     // 如果所有的分类复选框都未被选中
     if ($('.categories input[type="checkbox"]:checked').length === 0) {
       // 将“全部”复选框选中
