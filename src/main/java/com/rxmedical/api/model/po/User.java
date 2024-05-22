3f43b77173b2d9c3341f944df0834a7b1afabb69
JerryWang0122 <school832@gmail.com>
2024-05-22 20:07:45 +0800

    Feat: Update user po

--- src/main/java/com/rxmedical/api/model/po/User.java
+++ src/main/java/com/rxmedical/api/model/po/User.java
@@ -1,4 +1,7 @@
 package com.rxmedical.api.model.po;
 
+import java.util.Date;
+
 public record User(Integer id, String empId, String name, String password,
-                   String dept, String title, String email, String authLevel){}
+                   String dept, String title, String email, String authLevel,
+                   Date createDate, Date updateDate){}
