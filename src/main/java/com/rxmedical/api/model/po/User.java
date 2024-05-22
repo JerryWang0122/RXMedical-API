11a6eab0634bb1cd6b649cacff90f6c472e61259
JerryWang0122 <school832@gmail.com>
2024-05-22 17:03:05 +0800

    Feat: 使用record設定user po

--- src/main/java/com/rxmedical/api/model/po/User.java
+++ src/main/java/com/rxmedical/api/model/po/User.java
@@ -1,19 +1,4 @@
 package com.rxmedical.api.model.po;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class User {
-    private Integer id;
-    private String empId;	// 員工編號
-    private String name;	// 姓名
-    private String password; // 密碼
-    private String dept;	// 單位
-    private String title;	// 職稱
-    private String email;   // 信箱
-    private String authLevel; // 權限級別
-}
+public record User(Integer id, String empId, String name, String password,
+                   String dept, String title, String email, String authLevel){}
