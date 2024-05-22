37835cca708207b6d299fc343f17e117df7b6860
JerryWang0122 <school832@gmail.com>
2024-05-22 20:21:58 +0800

    docs: add Product po description

--- src/main/java/com/rxmedical/api/model/po/Product.java
+++ src/main/java/com/rxmedical/api/model/po/Product.java
@@ -2,5 +2,16 @@ package com.rxmedical.api.model.po;
 
 import java.util.Date;
 
-public record Product(Integer id, String name, Integer quantity, String desc,
+/**
+ *
+ * @param id
+ * @param name
+ * @param stock 庫存量
+ * @param desc 商品描述
+ * @param picture 圖片儲存位置
+ * @param category 商品種類
+ * @param createDate
+ * @param updateDate
+ */
+public record Product(Integer id, String name, Integer stock, String desc,
                       String picture, String category, Date createDate, Date updateDate) {}
