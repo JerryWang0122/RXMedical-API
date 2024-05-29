673191d628a6ba877b73fdc860138b3026ba7a9b
JerryWang0122 <school832@gmail.com>
2024-05-29 22:31:16 +0800

    feat: Mysql update

--- src/main/java/com/rxmedical/docs/rxMedical.sql
+++ src/main/java/com/rxmedical/docs/rxMedical.sql
@@ -3,10 +3,14 @@ use rxmedical;
 DROP TABLE IF EXISTS history;
 DROP TABLE IF EXISTS record;
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS product;
+
+-- User
+SELECT * FROM user; 
+
+INSERT INTO `user` (`emp_code`, dept, title, `name`, email, salt, password, auth_level, create_date, update_date) VALUES
+('00000', '衛材庫房', 'root', 'root', 'root@root.com', '8b4c873ac087c7a553642d528f60aa8e', '6e29fc2a04004b7a9aa1fe986309e49bf9e4123171993d896ad4a179565c7caa', 'root', NOW(), NOW()),
+('73174', '秘書室', '替代役', '王俊傑', 'school832@gmail.com', '94f725be236567bb57d77708ac9b45aa', 'caa6a26d3a35d0c99c161543cac20d2e68258a211993091d3ff6298ae52fe95f', 'admin', NOW(), NOW()),
+('12345', '文書中心', '一般專員', '金一蓉', 'test@test.com', '24d518fae3d305a2df5a1ca3aaeca769', 'f3f6446f9cb3f981912a21e25d33246019ddb650361f31fd1d8f5a431aa939ad', 'staff', NOW(), NOW());
 
 
-
-
-INSERT INTO `rxmedical`.`user` (`id`, `auth_level`, `create_date`, `update_date`, `dept`, `email`, `emp_id`, `name`, `password`, `title`) VALUES 
-('1', 'root', '2024-05-23', '2024-05-23', '衛材庫房', 'root@root', '00000', 'root', '123456', 'root'),
-('2', 'admin', '2024-05-23', '2024-05-23', '秘書室', 'school832@gmail.com', '73174', '王俊傑', '123456', '替代役');
