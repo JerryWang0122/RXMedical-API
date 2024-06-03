1171ac519f93d6074a732e572f6f3559cacaad34
JerryWang0122 <school832@gmail.com>
2024-06-03 17:19:07 +0800

    pre: 發送Email

---
+++ src/main/java/com/rxmedical/api/util/SendEmailUtil.java
@@ -0,0 +1,19 @@
+package com.rxmedical.api.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SendEmailUtil {
+
+    @Bean
+    public void sendEmail() {
+        System.out.println("發送郵件");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("測試用");
+
+
+    }
+}
